f00b4708504e08dfaeca0ae6e777e63a92d85527
justin <justinbourb@gmail.com>
2017-08-01 07:53:17 -0400

    Updated blog page.

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
+
 class PostsAdmin(admin.ModelAdmin):
     list_display = ('author', 'title')
     search_fields = ('title', 'text')
